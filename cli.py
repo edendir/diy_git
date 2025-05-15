c92fdf4ab3fca996c7340a417e3deb88d976173c
Robbie Lannon <robbielannon@gmail.com>
2025-05-15 10:00:13 -0500

    refs for each commit

--- cli.py
+++ cli.py
@@ -97,9 +97,14 @@ def commit(args):
 
 # Log the commits
 def log(args):
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
+    
     for oid in base.iter_commits_and_parents(args.oid):
         commit = base.get_commit(oid)
-        print(f'commit {oid}\n')
+        refs_str = f' ({", ".join(refs.get(oid, []))})' if refs.get(oid) else ''
+        print(f'commit {oid}{refs_str}\n')
         print(textwrap.indent(commit.message, '    '))
         print('')
 
